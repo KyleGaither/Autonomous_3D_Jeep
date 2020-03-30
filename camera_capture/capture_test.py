295b6335d95e6663e69d684ef6e9c730c92f5ccf
Kyle Gaither <kyle.gaither@gmail.com>
2020-03-30 17:08:39 -0400

    fix gstreamer pipeline for opencv

--- camera_capture/capture_test.py
+++ camera_capture/capture_test.py
@@ -1,8 +1,15 @@
 import numpy as np
 import cv2
-import jetson.utils
 
-cap = cv2.VideoCapture(0, cv.CAP_GSTREAMER)
+gStreamer_pipeline = "nvarguscamerasrc ! "
+					 "video/x-raw(memory:NVMM), "
+        			 "width=(int)3280, height=(int)2464, "
+        			 "format=(string)NV12, framerate=(fraction)21/1 ! "
+        			 "nvvidconv flip-method=2 ! "
+       			 	 "video/x-raw, width=1280, height=720 format=(string)BGRx ! "
+        			 "videoconvert ! "
+        			 "video/x-raw, format=(string)BGR ! appsink"
+cap = cv2.VideoCapture(gStreamer_pipeline, cv2.CAP_GSTREAMER)
 if not cap.isOpened():
 	print("Cannot open Camera")
 	exit()
@@ -15,7 +22,7 @@ while True:
 		print("Can't receive frame. Exiting.....")
 		break
 	#display the resulting frame 
-	cv2.imshow('frame', frame)
+	cv2.imshow("frame", frame)
 	if cv2.waitKey(1) == ord('q'):
 		break
 
