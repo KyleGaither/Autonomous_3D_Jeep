0818207f4463520a6906b063a2c9a6e65f206bf4
Kyle Gaither <kyle.gaither@gmail.com>
2020-04-02 22:47:44 -0400

    add gstreamer_pipeline_show and camera_show function to lib_autonomous and call within capture_test

--- camera_capture/capture_test.py
+++ camera_capture/capture_test.py
@@ -1,24 +1,5 @@
 import cv2
+import lib_autonomous_jeep
 
-cv2_pipeline ="nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink" 
-
-print(cv2_pipeline) 
-
-
-cap = cv2.VideoCapture(cv2_pipeline, cv2.CAP_GSTREAMER)
-if cap.isOpened():
-	window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-	# Window
-	while cv2.getWindowProperty("CSI Camera", 0) >= 0:
-		ret_val, img = cap.read()
-		cv2.imshow("CSI Camera",img)
-		# This also acts as
-		keyCode = cv2.waitKey(30) & 0xFF
-		# Stop the program on the ESC key
-		if keyCode == 27:
-			break
-	cap.release()
-	cv2.destroyAllWindows()
-else:
-	print("Unable to open camera")
+print(gstreamer_pipeline_show())
 
